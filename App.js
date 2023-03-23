a83f07faff5f9e3f271fb9cfe3475bf8aeb6adfa
Olli-Pekka <n1haol00@students.oamk.fi>
2023-03-23 11:51:23 +0200

    Added initial components

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './components';
+import Login from './components';
+import Profile from './components';
+import Round from './components';
+import Scores from './components';
+import Search from './components';
+import styles from './style/styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Disc golf companion app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Scores.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default Scores = () => {
+    return (
+        <View>
+
+        </View>
+    );
+}
